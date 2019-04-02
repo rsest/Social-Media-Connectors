08b41c4f0ed77be815273191930f2fcc3f22faa0
Rodrigo Sestari <rodrigo.sestari@infinitec.solutions>
2019-04-02 15:21:45 +0200

    klar

--- twitterSearch.py
+++ twitterSearch.py
@@ -20,17 +20,19 @@ if save_places:
         json.dump(places, outfile, indent=2)
         # api.reverse_geocode()
         # https: // developer.twitter.com / en / docs / geo / places - near - location / api - reference / get - geo - reverse_geocode
-trends = api.trends_place(23424853)
+trends = api.trends_place(116545)
 print(json.dumps(trends[0], indent=4))
+pos = api.geo_search(lat=19.4326,long=-99.1332)
 
-search_results = api.search(q="#BahrainGP", count=100, include_entities=True)
-search_hashtag = tweepy.Cursor(api.search, q='hashtag', include_entities=True).items(5000)
+search_results = api.search(q="klar", geocode='19.4326,-99.133,30mi', count=30, include_entities=True)
+search_hashtag = tweepy.Cursor(api.search, q="klar", geocode='19.4326,-99.133,200mi', include_entities=True).items(5000)
 for tweet in search_hashtag:
     print(tweet.text)
     print(tweet.entities['urls'])
     if 'media' in tweet.entities:
         for image in tweet.entities['media']:
             print(image['media_url'])
+
 
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
